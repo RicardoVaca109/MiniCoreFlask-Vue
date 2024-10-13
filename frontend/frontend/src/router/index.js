c060b5c37587ac9dcbc49359c485026a514d82e9
Ricardo <ricardo.vaca@udla.edu.ec>
2024-10-13 16:46:38 -0500

    Configuración Rutas Prueba

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,30 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
-
+import SharkComponent from "@/components/SharkComponent.vue";
+import CarsComponent from "@/components/CarsComponent.vue";
 Vue.use(VueRouter);
-
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "/shark",
+    name: "SharkComponent",
+    component: SharkComponent,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/cars",
+    name: "CarsComponent",
+    component: CarsComponent,
   },
 ];
-
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
-
 export default router;
