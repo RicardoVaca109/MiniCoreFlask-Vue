b617b7e004ea143740917690d65f094b08223b5a
Ricardo <ricardo.vaca@udla.edu.ec>
2024-10-14 14:20:15 -0500

    Llamar a los controladores usando blue print

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 # Importar librerias a usarse
 from flask import Flask, jsonify
 from flask_cors import CORS
+from controllers.greetings_controller import greetings_bp
+from controllers.shark_controller import shark_bp
+from controllers.vehiculos_controllers import vehiculos_bp
+from controllers.propietarios_controller import propietarios_bp
 
 # Instanciamos una aplicacion de tipo FLask
 app = Flask(__name__)
@@ -11,10 +15,23 @@ app.config.from_object(__name__)
 # Habilitar CORS (Cross Origin Resource Sharing)
 CORS(app, resources={r"/*":{'origins':"*"}}) 
 
-# Ruta para un Hello World
-@app.route('/', methods = ['GET'])
-def greetings():
-    return('Hello World!')
+app.register_blueprint(greetings_bp)
+app.register_blueprint(shark_bp)
+app.register_blueprint(vehiculos_bp)
+app.register_blueprint(propietarios_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+# # Ruta para un Hello World
+# @app.route('/', methods = ['GET'])
+# def greetings():
+#     return('Hello World!')
+
+# app.register_blueprint(greetings_bp)
+# app.register_blueprint(shark_bp)
+
+# # Ruta para shark y usar Axios para conectar al frontend
+# @app.route('/shark', methods = ['GET'])
+# def shark():
+#     return('Esto es shark')    
